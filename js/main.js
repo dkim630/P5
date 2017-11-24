f80d7d116d693929c5eb9ca8a002752af26b7163
bmock96 <b.mock96@gmail.com>
2017-11-24 13:27:27 -0500

    overview axis and resized svg

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+//SVG Parameters
+var svg = d3.select('svg');
+var svgWidth = +svg.attr('width');
+var svgHeight = +svg.attr('height');
+//Padding if needed
+var padding = {t: 40, r: 60, b: 40, l: 60};
+//chartG set up
+var chartG = svg.append('g')
+    .attr('transform', 'translate('+[padding.l, padding.t]+')');
+//Overview Chart Set Up
+var overviewHeight =400;
+var overviewWidth = 400;
+//scales
+var xScaleOverview = d3.scaleLinear().range([0,overviewWidth]);
+var yScaleOverview = d3.scaleLinear().range([overviewHeight,0]);
+//add more
 
 
 d3.csv('./data/movies-title-edited.csv',
@@ -5,30 +21,30 @@ function(row){
     // This callback formats each row of the data
     return {
         director_name : row.director_name,
-        num_critic_for_reviews : row.num_critic_for_reviews,
-        duration : row.duration,
-        director_facebook_likes : row.director_facebook_likes,
+        num_critic_for_reviews : +row.num_critic_for_reviews,
+        duration : +row.duration,
+        director_facebook_likes : +row.director_facebook_likes,
         actor_1_name : row.actor_1_name,
-        actor_1_facebook_likes : row.actor_1_facebook_likes,
+        actor_1_facebook_likes : +row.actor_1_facebook_likes,
         actor_2_name : row.actor_2_name,
-        actor_2_facebook_likes: row.actor_2_facebook_likes,
+        actor_2_facebook_likes: +row.actor_2_facebook_likes,
         actor_3_name: row.actor_3_name,
-        actor_3_facebook_likes : row.actor_3_facebook_likes,
-        gross : row.gross,
+        actor_3_facebook_likes : +row.actor_3_facebook_likes,
+        gross : +row.gross,
         genres : row.genres,
         movie_title : row.movie_title,
-        num_voted_users : row.num_voted_users,
-        cast_total_facebook_likes : row.cast_total_facebook_likes,
+        num_voted_users : +row.num_voted_users,
+        cast_total_facebook_likes : +row.cast_total_facebook_likes,
         plot_keywords : row.plot_keywords,
         movie_imdb_link : row.movie_imdb_link,
-        num_user_for_reviews : row.num_user_for_reviews,
+        num_user_for_reviews : +row.num_user_for_reviews,
         language : row.language,
         country : row.country,
         content_rating : row.content_rating,
-        budget : row.budget,
-        title_year : row.title_year,
-        imdb_score : row.imdb_score,
-        movie_facebook_likes : row.movie_facebook_likes
+        budget : +row.budget,
+        title_year : +row.title_year,
+        imdb_score : +row.imdb_score,
+        movie_facebook_likes : +row.movie_facebook_likes
 
     }
 },
@@ -40,6 +56,28 @@ function(error, dataset){
     }
 
     console.log(dataset);
+
+    //axis setup for first overview dot plot
+    var grossExtent = d3.extent(dataset, function(d) {
+        return d['gross'];
+        });
+    xScaleOverview.domain(grossExtent);
+    console.log(grossExtent);
+    chartG.append('g')
+        .attr('class','x axis')
+        .attr('transform', 'translate(' +[0,overviewHeight]+' )')
+        .call(d3.axisBottom(xScaleOverview).ticks(8));
+
+    var votedExtent = d3.extent(dataset, function(d) {
+        return d['num_voted_users'];
+        });
+    console.log(votedExtent);
+    yScaleOverview.domain(votedExtent);
+    chartG.append('g')
+        .attr('class','y axis')
+        .attr('transform', 'translate(' +[0,0]+' )')
+        .call(d3.axisLeft(yScaleOverview).ticks(10));
+
 
 });
 
